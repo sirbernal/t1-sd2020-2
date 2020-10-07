27736bd0411c9895fda20ea0fa2146a4211f0be5
Cristian Bernal <cristian.bernal@sansano.usm.cl>
2020-10-06 23:22:15 -0300

    primer commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/sirbernal/t1-sd2020-2
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	google.golang.org/grpc v1.32.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.0.0 // indirect
+)
---
+++ logistica/logistica.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	fmt.Println("Prueba del server logistica")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := chat.Server{}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
