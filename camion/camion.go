026988a3912863f733baeb33f7cf39ce3aa4badb
Raul Alvarez <raul.alvarezc@sansano.usm.cl>
2020-10-14 21:01:25 -0300

    agregado simulacion viaje

--- camion/camion.go
+++ camion/camion.go
@@ -3,9 +3,11 @@ import (
 	"context"
 	"log"
 	"fmt"
-	//"time"
+	"time"
+	"reflect"
 	pb "github.com/sirbernal/t1-sd2020-2/proto/camion_logistica"
 	"google.golang.org/grpc"
+	"math/rand"
 )
 
 const (
@@ -13,12 +15,68 @@ const (
 )
 
 type Envio struct{
-	id string
-	producto string
+	idPaquete string
+	seguimiento int64
+	tipo int64 //{normal, prioritario, retail}={0,1,2}
 	valor int64
-	tienda string
-	destino string
-	prioritario int64 //{normal, prioritario, retail}={0,1,2}
+	intentos int64
+	estado int64 //{En bodega, En camino, Recibido o No Recibido}={0,1,2,3}
+	fecha_entrega time.Time
+}
+
+func menorEnvio(x Envio, y Envio)(Envio, Envio){
+	if x.valor>y.valor{
+		return x,y
+	}else{
+		return y,x
+	}
+}
+func viaje(env [2]Envio)[2]Envio{
+	void:= Envio{
+		estado:1,
+	}
+	for i,pac := range env{
+		if reflect.DeepEqual(pac,void){
+			continue
+		}
+		for x:=0;x<4;x++{
+			if x==3{
+				pac.estado=3
+				break
+			}
+			if pac.tipo<2 && int64(x)*10>pac.valor{
+				pac.estado=3
+				break
+			}
+			pac.intentos++
+			rand.Seed(time.Now().UnixNano())
+			probabilidad:= rand.Intn(101)
+			if probabilidad<80{
+				pac.estado=2
+				pac.fecha_entrega=time.Now()
+				break
+			}
+		}
+		env[i]=pac
+	}
+	return env
+}
+func simularEnvio(env [6]Envio){
+	e0,e1 := menorEnvio(env[0],env[1])
+	e2,e3 := menorEnvio(env[2],env[3])
+	e4,e5 := menorEnvio(env[4],env[5])
+	cam1 :=[2]Envio{e0,e1} //camion retail 1
+	cam2 :=[2]Envio{e2,e3}//camion retail 2
+	cam3 :=[2]Envio{e4,e5} //camion normal
+	fmt.Println(cam1)
+	fmt.Println(cam2)
+	fmt.Println(cam3)
+	cam1 = viaje(cam1)
+	cam2 = viaje(cam2)
+	cam3 = viaje(cam3)
+	fmt.Println(cam1)
+	fmt.Println(cam2)
+	fmt.Println(cam3)
 }
 
 func main() {
@@ -31,15 +89,16 @@ func main() {
 	if err != nil {
 		log.Fatalf("Conn err: %v", err)
 	}
-	
+	var envios [6]Envio
 	client := pb.NewCamionServiceClient(conn)
 	stream, err := client.Camion(context.Background())
 	waitc := make(chan struct{})
 
 	msg := &pb.CamionRequest{Status: "listo pa marakear" }
 	go func() {
-		for i := 0; i <= 1; i++ {
+		for i := 0; i < 2; i++ {
 		stream.Send(msg) }
+			
 		/*for i := 1; i <= 10; i++ {
 			log.Println("Sleeping...")
 			time.Sleep(2 * time.Second)
@@ -49,13 +108,30 @@ func main() {
 	}()
 	//time.Sleep(10 * time.Second)
 	go func() {
+		npack:= 0
 		for {
 			resp, err := stream.Recv()
 			fmt.Println(resp.IdPaquete)
 			fmt.Println(resp.Intentos)
+			fmt.Println(npack)
+			paquete := Envio{
+				idPaquete : resp.IdPaquete,
+				seguimiento : resp.Seguimiento,
+				tipo :resp.Tipo,
+				valor :  resp.Valor,
+				intentos : resp.Intentos,
+				estado : 1,
+			}
+			envios[npack]=paquete
 			if err != nil {
 				log.Fatalf("can not receive %v", err)
 			}
+			npack++
+			if npack>5{
+				npack=0
+				simularEnvio(envios)
+			}
+			//fmt.Println(envios)
 			
 			
 		}
