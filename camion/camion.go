534f5962b321688acb5b524bdbc25ceeaeb5f02b
Cristian Bernal <cristian.bernal@sansano.usm.cl>
2020-10-18 22:07:47 -0300

    agregado comentarios en camion.go

--- camion/camion.go
+++ camion/camion.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	dire = "10.10.28.82:50051"
+	dire = "10.10.28.82:50051"   //Direccion maquina logistica para hacer coneccion grpc
 )
 
 type Envio struct{
@@ -34,7 +34,7 @@ var tiempocamiones string
 var tiempocamionesint int
 var tiempoentrega string
 var tiempoentregaint int
-func updateCamion(result [6]Envio){
+func updateCamion(result [6]Envio){    //Funcion que guarda los registros de los envios de los paquetes en archivos csv
 	for i,pack := range result{
 		if reflect.DeepEqual(pack,Envio{estado:1}){
 			continue
@@ -122,17 +122,17 @@ func updateCamion(result [6]Envio){
 	
 
 }
-func menorEnvio(x Envio, y Envio)(Envio, Envio){
+func menorEnvio(x Envio, y Envio)(Envio, Envio){   
 	if x.valor>y.valor{
 		return x,y
 	}else{
 		return y,x
 	}
 }
-func viaje(env [2]Envio)[2]Envio{
-	void:= Envio{
-		estado:1,
-	}
+func viaje(env [2]Envio)[2]Envio{  //funcion invocada al hacer una simulacion, lo que realizara en teoria el viaje
+	void:= Envio{             // por camion (osea 2 paquetes) y retornara los resutados de los paquetes tras el viaje
+		estado:1,         // Es aqui donde funcionan las probabilidades mencionadas en el enunciado y el hacer intentos
+	}                         // de entrega
 	for i,pac := range env{
 		if reflect.DeepEqual(pac,void){
 			continue
@@ -159,8 +159,8 @@ func viaje(env [2]Envio)[2]Envio{
 	}
 	return env
 }
-func simularEnvio(env [6]Envio)[6]Envio{ 
-	e0,e1 := menorEnvio(env[0],env[1])
+func simularEnvio(env [6]Envio)[6]Envio{    // Estan función recibe el arreglo de 6 paquetes, para simular los envios 
+	e0,e1 := menorEnvio(env[0],env[1])  // en cada camion y retornará el arreglo con los resultados de la simulacion
 	e2,e3 := menorEnvio(env[2],env[3])
 	e4,e5 := menorEnvio(env[4],env[5])
 	cam1 :=[2]Envio{e0,e1} //camion retail 1
@@ -176,8 +176,9 @@ func simularEnvio(env [6]Envio)[6]Envio{
 }
 
 func main() {
+	// En estas primeras lineas se pide los tiempos de espera y demora de los camiones
 	fmt.Print("Inicializando servicio de reparto...\nIngrese tiempo (segundos) de espera de camiones:")
-	for{
+	for {
 		fmt.Scanln(&tiempocamiones)
 		intmode, err:=strconv.Atoi(tiempocamiones)
 		if err!=nil{
@@ -198,47 +199,39 @@ func main() {
 		tiempoentregaint=intmode
 		break
 	}
-	//fmt.Println("Usuario del sistema PrestigioExpress, por favor ingresar el tiempo de envio de pedidos a logistica (en segundos): ")
-	//var tiempo int64
-	//fmt.Scanln(&tiempo)  
+
 	// Creando conexion TCP
 	conn, err := grpc.Dial(dire, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Conn err: %v", err)
 	}
 	var envios [6]Envio
-	client := pb.NewCamionServiceClient(conn)
-	stream, err := client.Camion(context.Background())
-	waitc := make(chan struct{})
+	client := pb.NewCamionServiceClient(conn)  // Se define la conexion con el servicio definido en el proto 
+	stream, err := client.Camion(context.Background()) // Como se uso un stream, se inicializa aca
+	waitc := make(chan struct{})  // Se define una variable de canal que nos permite mantener las goroutines activas durante 
+	// todo el proceso
 
-	msg := &pb.CamionRequest{IdPaquete : "",
+	// La logica del camion primero es enviar un paquete "vacio" para avisarle a logistica que este servicio esta activo
+	// para hacer andar los camiones
+
+	msg := &pb.CamionRequest{IdPaquete : "",     
 		Seguimiento : -1,
 		Tipo :0,
 		Valor :  0,
 		Intentos : 0,
 		Estado : 0,}
-	go func() {
-		for i := 0; i < 1; i++ {
+	go func() {    // Enviamos nuestro primer mensaje que mencionamos anteriormente en esta goroutine
+		for i := 0; i < 1; i++ {  
 		stream.Send(msg) }
-			
-		/*for i := 1; i <= 10; i++ {
-			log.Println("Sleeping...")
-			time.Sleep(2 * time.Second)
-			log.Println("Sending msg...")
-			stream.Send(msg)
-		}*/
+		
 	}()
-	//time.Sleep(10 * time.Second)
-	go func() {
+	go func() {  // Esta goroutine estara encargada basicamente de recibir paquetes de logistica y hacer todo
 		npack:= 0
-		var resultado [6]Envio
-		for {
+		var resultado [6]Envio // Para guardar los paquetes que se reciben
+		for {     // Recibimos los paquetes de logistica
 			resp, err := stream.Recv()
-			
-			//fmt.Println(resp.IdPaquete)
-			//fmt.Println(resp.Intentos)
 			fmt.Println(npack)
-			paquete := Envio{
+			paquete := Envio{          // los paquetes los guardamos en esta clase
 				idPaquete : resp.IdPaquete,
 				seguimiento : resp.Seguimiento,
 				tipo :resp.Tipo,
@@ -246,54 +239,38 @@ func main() {
 				intentos : resp.Intentos,
 				estado : 1,
 			}
-			envios[npack]=paquete
+			envios[npack]=paquete  // Se guarda cada paquete en el arreglo de arriba
 			if err != nil {
 				log.Fatalf("can not receive %v", err)
 			}
 			npack++
-			if npack==6{
-				resultado = simularEnvio(envios)
-				for _, pack := range resultado{
-					msg2 := &pb.CamionRequest{
+			if npack==6{  // Al recibir todos los 6 paquetes correspondientes a 2 de cada camion, se simula los envios
+				resultado = simularEnvio(envios)  // Simulamos envios, definido arriba. Recibiremos un arreglo con los resultados
+				for _, pack := range resultado{ // Paso siguiente: enviar devuelta cada resultado de los envios de los paquetes
+					msg2 := &pb.CamionRequest{    // definimos lo que vamos a enviar por cada paquete
 						IdPaquete: pack.idPaquete,   
 						Seguimiento: pack.seguimiento,
 						Tipo: pack.tipo,        
 						Valor: pack.valor,       
 						Intentos: pack.intentos,    
 						Estado: pack.estado }
-					stream.Send(msg2)
-					time.Sleep(time.Duration(tiempoentregaint*1000)*time.Millisecond)
+					stream.Send(msg2) // enviamos
+					time.Sleep(time.Duration(tiempoentregaint*1000)*time.Millisecond) // Tiempo de espera definido arriba x el usuario
 				}
 				npack++
 			}
-			if npack==7{
-				stream.Send(msg)
-				npack=0
+			if npack==7{    // Cuando terminamos de hacer el recebimieno, simulacion y entrega de resultados de los 6 
+				stream.Send(msg)  //paquetes, nuevamente usamos nuestro mensaje inicial para avisar a logistica que podemos
+				npack=0        // recibir 6 paquetes mas
 			}
-			time.Sleep(time.Duration(tiempocamionesint*1000)*time.Millisecond)
+			time.Sleep(time.Duration(tiempocamionesint*1000)*time.Millisecond) // Tiempo de espera que se definio al principio
 			//fmt.Println(envios)
 		}
 
 
 	}()
-	<-waitc
+	<-waitc  // esto en teoria permite que las goroutines funcionen todo el tiempo (segun internet)
 	stream.CloseSend()
 }
 
-
-	/* c := cl.NewSeguimientoServiceClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	envio := &cl.SeguimientoRequest{
-		Msg: "wea",
-		Msg2: "wea2",
-	}
-	r, err := c.Seguimiento(ctx, envio)
-
-	if err != nil {
-		log.Fatalf("Requ err: %v", err)
-	}
-
-	log.Println("Respuesta : ", r.GetConfirmation()) */
 
